5a9d35c8dda5a3a5467f6b81fa18973c2785b46a
juliagbmaciel <juliagabrielle863@gmail.com>
2024-02-27 11:21:48 -0300

    add api post to script

--- Assets/Scripts/SliderHander.cs
+++ Assets/Scripts/SliderHander.cs
@@ -1,17 +1,51 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+
+
 
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos
 {
     [AddComponentMenu("Scripts/MRTK/Examples/ShowSliderValue")]
     public class ControlUR5 : MonoBehaviour
     {
+        private const string apiUrl = "http://127.0.0.1:8000/receive";
+
         public void OnSliderUpdated(SliderEventData eventData)
         {
-            this.transform.rotation = Quaternion.Euler(0, 0, eventData.NewValue * 100f);
+            transform.rotation = Quaternion.Euler(0, 0, eventData.NewValue * 100f);
+            Debug.Log(this);
+
+            if (eventData.NewValue > 0.5)
+            {
+                float x = 1.0f;
+                float y = 2.0f;
+                float z = 3.0f;
+
+                string json = $"{{\"x\": {x}, \"y\": {y}, \"z\": {z}}}";
+
+                StartCoroutine(SendPostRequest(json));
+            }
         }
+
+        IEnumerator SendPostRequest(string json)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/receive", json , "application/json"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
     }
 }
