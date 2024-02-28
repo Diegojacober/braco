3afd877b9b90d708c962b87900687afcc9947c87
OFelipeMeira <felipecmeira2004@gmail.com>
2024-02-28 01:02:52 -0300

    sliders updated

--- Assets/Scripts/SliderHander.cs
+++ Assets/Scripts/SliderHander.cs
@@ -10,42 +10,83 @@ using System.Collections.Generic;
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos
 {
     [AddComponentMenu("Scripts/MRTK/Examples/ShowSliderValue")]
+
+
+
     public class ControlUR5 : MonoBehaviour
     {
-        private const string apiUrl = "http://127.0.0.1:8000/receive";
+        // private const string apiUrl = "http://127.0.0.1:8000/receive";
+
+
+        void Start()
+        {
+            if (this.tag == "X")
+            {
+                transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
+            }
+        }
 
         public void OnSliderUpdated(SliderEventData eventData)
         {
-            transform.rotation = Quaternion.Euler(0, 0, eventData.NewValue * 100f);
-            Debug.Log(this);
 
-            if (eventData.NewValue > 0.5)
+            if (this.tag == "Z")
             {
-                float x = 1.0f;
-                float y = 2.0f;
-                float z = 3.0f;
-
-                string json = $"{{\"x\": {x}, \"y\": {y}, \"z\": {z}}}";
-
-                StartCoroutine(SendPostRequest(json));
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, eventData.NewValue * 360f);
             }
+            if (this.tag == "X")
+            {
+                transform.rotation = Quaternion.Euler(eventData.NewValue * 360f, 90.0f, 90.0f);
+                // transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self)
+            }
+            // switch (this.tag)
+            // {
+            //     case "Z":
+            //         transform.rotation = Quaternion.Euler(1, 1, eventData.NewValue * 360f);
+            //         break;
+
+            //     case "X":
+            //         transform.rotation = Quaternion.Euler(eventData.NewValue * 360f, 1, 1);
+            //         Debug.Log(transform.localRotation.eulerAngles.x);
+            //         Debug.Log(transform.localRotation.eulerAngles.y);
+            //         Debug.Log(transform.localRotation.eulerAngles.z);
+            //         break;
+
+            //     case "Y":
+            //         transform.rotation = Quaternion.Euler(1, eventData.NewValue * 360f, 1);
+            //         break;
+
+            //     default:
+            //         Debug.Log("Ta errado");
+            //         break;
+            // }
+
+            // if (eventData.NewValue > 0.5)
+            // {
+            //     float x = 1.0f;
+            //     float y = 2.0f;
+            //     float z = 3.0f;
+
+            //     string json = $"{{\"x\": {x}, \"y\": {y}, \"z\": {z}}}";
+
+            //     StartCoroutine(SendPostRequest(json));
+            // }
         }
 
-        IEnumerator SendPostRequest(string json)
-    {
-        using (UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/receive", json , "application/json"))
-        {
-            yield return www.SendWebRequest();
+        // IEnumerator SendPostRequest(string json)
+        // {
+        //     using (UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/receive", json, "application/json"))
+        //     {
+        //         yield return www.SendWebRequest();
 
-            if (www.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError(www.error);
-            }
-            else
-            {
-                Debug.Log("Form upload complete!");
-            }
-        }
-    }
+        //         if (www.result != UnityWebRequest.Result.Success)
+        //         {
+        //             Debug.LogError(www.error);
+        //         }
+        //         else
+        //         {
+        //             Debug.Log("Form upload complete!");
+        //         }
+        //     }
+        // }
     }
 }
